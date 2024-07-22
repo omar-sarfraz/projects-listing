9224ae0f1c8b6f6ebc0e84be8e4f4855b2338500
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-07-22 14:20:19 +0500

    [Change] Component code in seperate filees

--- React Without Tooling/ProjectList.js
+++ React Without Tooling/ProjectList.js
@@ -1,5 +1,3 @@
-const e = React.createElement;
-
 function ProjectsList() {
     const [projects, setProjects] = React.useState();
 
@@ -13,77 +11,46 @@ function ProjectsList() {
         setProjects(projects);
     };
 
-    return e(
-        "ul",
-        { id: "list", className: "flex flex-wrap justify-start gap-2" },
+    return (
         projects?.length &&
-            projects.map((project, index) =>
-                e(
-                    "li",
-                    {
-                        className:
-                            "relative bg-gray-100 flex flex-col grow justify-between rounded-md p-4 sm:w-full lg:w-1/4 group",
-                        key: index,
-                    },
-                    e(
-                        "div",
-                        null,
-                        e(
-                            "h3",
-                            { className: "text-2xl font-medium" },
-                            `${project.ProjectId}: ${project.Name}`
-                        ),
-                        e("div", { className: "my-4" }, project.Description)
-                    ),
-                    e(
-                        "div",
-                        null,
-                        e(
-                            "div",
-                            {
-                                className:
-                                    "flex w-full justify-between group-hover:opacity-0 transition duration-500",
-                            },
-                            e(
-                                "div",
-                                null,
-                                e("h3", { className: "font-bold" }, "Budget"),
-                                e(
-                                    "div",
-                                    { className: "bg-cyan-500 rounded-full px-3 mt-1 text-white" },
-                                    project.Budget
-                                )
-                            ),
-                            e(
-                                "div",
-                                null,
-                                e("h3", { className: "font-bold" }, "Timeline"),
-                                e(
-                                    "div",
-                                    {
-                                        className:
-                                            "bg-emerald-500 rounded-full px-3 mt-1 text-white",
-                                    },
-                                    project.Timeline
-                                )
-                            )
-                        ),
-                        e(
-                            "a",
-                            {
-                                className:
-                                    "absolute left-0 w-full cursor-pointer bg-gradient-to-r from-cyan-500 to-emerald-500 text-white rounded-md text-center py-2 mt-4 opacity-0 group-hover:-translate-y-full group-hover:opacity-100 transition duration-500 ease-in-out",
-                            },
-                            "Apply Now"
-                        )
-                    )
-                )
-            )
+        projects.map((project, index) => (
+            <li
+                className="relative bg-gray-100 flex flex-col grow justify-between rounded-md p-4 sm:w-full lg:w-1/4 group"
+                key={index}
+            >
+                <div>
+                    <h3 className="text-2xl font-medium">
+                        {project.ProjectId}: {project.Name}
+                    </h3>
+                    <div className="my-4">
+                        <div className="font-bold">Description:</div>
+                        {project.Description}
+                    </div>
+                </div>
+                <div>
+                    <div className="flex w-full justify-between group-hover:opacity-0 transition duration-500">
+                        <div>
+                            <h3 className="font-bold">Budget</h3>
+                            <div className="bg-cyan-500 rounded-full px-3 mt-1 text-white">
+                                {project.Budget}
+                            </div>
+                        </div>
+                        <div>
+                            <h3 className="font-bold">Timeline</h3>
+                            <div className="bg-emerald-500 rounded-full px-3 mt-1 text-white">
+                                {project.Timeline}
+                            </div>
+                        </div>
+                    </div>
+                    <a className="absolute left-0 w-full cursor-pointer bg-gradient-to-r from-cyan-500 to-emerald-500 text-white rounded-md text-center py-2 mt-4 opacity-0 group-hover:-translate-y-full group-hover:opacity-100 transition duration-500 ease-in-out">
+                        Apply Now
+                    </a>
+                </div>
+            </li>
+        ))
     );
 }
 
-window.addEventListener("DOMContentLoaded", function () {
-    const domContainer = document.querySelector("#list");
-    const root = ReactDOM.createRoot(domContainer);
-    root.render(e(ProjectsList));
-});
+const domContainer = document.querySelector("#list");
+const root = ReactDOM.createRoot(domContainer);
+root.render(<ProjectsList />);
--- React Without Tooling/projects/AddProject.js
+++ React Without Tooling/projects/AddProject.js
@@ -1,5 +1,3 @@
-const e = React.createElement;
-
 function AddProject() {
     const [id, setId] = React.useState();
     const [name, setName] = React.useState();
@@ -44,113 +42,91 @@ function AddProject() {
         }
     };
 
-    return e(
-        "form",
-        { className: "flex flex-col gap-4 items-end bg-gray-100 px-6 py-10 rounded-md" },
-        e(
-            "div",
-            { className: "flex flex-col w-full items-center md:flex-row" },
-            e(
-                "label",
-                { className: "w-full md:w-1/3 text-xl", htmlFor: "project_id" },
-                "Project Id"
-            ),
-            e("input", {
-                className: "w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none",
-                name: "ProjectId",
-                id: "project_id",
-                type: "number",
-                placeholder: "e.g., 1",
-                required: true,
-                onChange: (e) => setId(parseInt(e.target.value)),
-            })
-        ),
-        e(
-            "div",
-            { className: "flex flex-col w-full items-center md:flex-row" },
-            e(
-                "label",
-                { className: "w-full md:w-1/3 text-xl", htmlFor: "project_name" },
-                "Project Name"
-            ),
-            e("input", {
-                className: "w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none",
-                name: "Name",
-                id: "project_name",
-                type: "text",
-                placeholder: "e.g., Ecommerce Website",
-                required: true,
-                onChange: (e) => setName(e.target.value),
-            })
-        ),
-        e(
-            "div",
-            { className: "flex flex-col w-full items-center md:flex-row" },
-            e(
-                "label",
-                { className: "w-full md:w-1/3 text-xl", htmlFor: "project_budget" },
-                "Project Budget"
-            ),
-            e("input", {
-                className: "w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none",
-                name: "Budget",
-                id: "project_budget",
-                type: "text",
-                placeholder: "e.g., 200 USD",
-                required: true,
-                onChange: (e) => setBudget(e.target.value),
-            })
-        ),
-        e(
-            "div",
-            { className: "flex flex-col w-full items-center md:flex-row" },
-            e(
-                "label",
-                { className: "w-full md:w-1/3 text-xl", htmlFor: "project_timeline" },
-                "Project Timeline"
-            ),
-            e("input", {
-                className: "w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none",
-                name: "Timeline",
-                id: "project_timeline",
-                type: "text",
-                placeholder: "e.g., 2 Months",
-                required: true,
-                onChange: (e) => setTimeline(e.target.value),
-            })
-        ),
-        e(
-            "div",
-            { className: "flex flex-col w-full items-center md:flex-row" },
-            e(
-                "label",
-                { className: "w-full md:w-1/3 text-xl", htmlFor: "project_description" },
-                "Project Description"
-            ),
-            e("textarea", {
-                className: "w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none",
-                name: "Description",
-                id: "project_description",
-                placeholder: "e.g., I want you to create an Clothing Ecommerce Store using React",
-                required: true,
-                onChange: (e) => setDescription(e.target.value),
-            })
-        ),
-        e(
-            "button",
-            {
-                type: "submit",
-                className:
-                    "bg-green-600 font-semibold text-white text-md px-4 py-2 rounded-md w-full md:w-1/3 mt-4 outline-none",
-                onClick: handleProjectSubmit,
-            },
-            "Submit"
-        )
+    return (
+        <form
+            className="flex flex-col gap-4 items-end bg-gray-100 px-6 py-10 rounded-md"
+            id="add_form"
+        >
+            <div className="flex flex-col w-full items-center md:flex-row">
+                <label className="w-full md:w-1/3 text-xl" htmlFor="project_id">
+                    Project Id
+                </label>
+                <input
+                    className="w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none"
+                    name="ProjectId"
+                    id="project_id"
+                    type="number"
+                    placeholder="e.g., 1"
+                    required
+                    onChange={(e) => setId(parseInt(e.target.value))}
+                />
+            </div>
+            <div className="flex flex-col w-full items-center md:flex-row">
+                <label className="w-full md:w-1/3 text-xl" htmlFor="project_name">
+                    Project Name
+                </label>
+                <input
+                    className="w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none"
+                    name="Name"
+                    id="project_name"
+                    type="text"
+                    placeholder="e.g., Ecommerce Website"
+                    required
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-col w-full items-center md:flex-row">
+                <label className="w-full md:w-1/3 text-xl" htmlFor="project_budget">
+                    Project Budget
+                </label>
+                <input
+                    className="w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none"
+                    name="Budget"
+                    id="project_budget"
+                    type="text"
+                    placeholder="e.g., 500 USD"
+                    required
+                    onChange={(e) => setBudget(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-col w-full items-center md:flex-row">
+                <label className="w-full md:w-1/3 text-xl" htmlFor="project_timeline">
+                    Project Timeline
+                </label>
+                <input
+                    className="w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none"
+                    name="Timeline"
+                    id="project_timeline"
+                    type="text"
+                    placeholder="e.g., 4 months"
+                    required
+                    onChange={(e) => setTimeline(e.target.value)}
+                />
+            </div>
+            <div className="flex flex-col w-full items-start md:flex-row">
+                <label className="w-full md:w-1/3 text-xl" htmlFor="project_description">
+                    Project Description
+                </label>
+                <textarea
+                    className="w-full md:w-2/3 border border-gray-300 rounded-md p-2 outline-none"
+                    id="project_description"
+                    name="Description"
+                    placeholder="e.g., I want you to create an Clothing Ecommerce Store using React"
+                    required
+                    onChange={(e) => setDescription(e.target.value)}
+                ></textarea>
+            </div>
+            <button
+                type="submit"
+                className="bg-green-600 font-semibold text-white text-md px-4 py-2 rounded-md w-full md:w-1/3 mt-4 outline-none"
+                onClick={handleProjectSubmit}
+            >
+                Submit
+            </button>
+        </form>
     );
 }
 
-window.addEventListener("DOMContentLoaded", function () {
-    const domContainer = document.querySelector("#add_form");
-    const root = ReactDOM.createRoot(domContainer);
-    root.render(e(AddProject));
-});
+const domContainer = document.querySelector("#add_form");
+const root = ReactDOM.createRoot(domContainer);
+root.render(<AddProject />);
