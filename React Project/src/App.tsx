99876678da99a4af86d6a683d9dad4560049b516
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-07-26 12:15:58 +0500

    [Changed] Location of NotFound.tsx

--- React Project/src/App.tsx
+++ React Project/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProjectsList from "./pages/ProjectsList/ProjectList";
 import AddProject from "./pages/AddProject/AddProject";
 import Layout from "./pages/Layout";
-import NotFound from "./components/NotFound";
+import NotFound from "./pages/NotFound";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 
