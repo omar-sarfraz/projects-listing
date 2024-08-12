32d0480cad5830173e47054e9830daf75ae9305c
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-08-12 10:51:42 +0500

    feat(backend): add a route to get project by id

--- Backend/routes/projects.ts
+++ Backend/routes/projects.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { addProject } from "../controllers/projects/addProject";
 import passport from "passport";
 import getProjects from "../controllers/projects/getProjects";
+
 import isClient from "../middlewares/isClient";
+import getProjectById from "../controllers/projects/getProjectById";
 
 const router = Router();
 
 router.post("/", passport.authenticate("jwt", { session: false }), isClient, addProject);
 router.get("/", passport.authenticate("jwt", { session: false }), getProjects);
+router.get("/:id", passport.authenticate("jwt", { session: false }), getProjectById);
 
 export default router;
