e7348e71dff786108e65b9e812776d9933b878a0
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-09-09 13:03:53 +0500

    feat(frontend): add basic pwa setup

--- React Project/vite.config.ts
+++ React Project/vite.config.ts
@@ -2,11 +2,31 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        VitePWA({
+            injectRegister: "auto",
+            workbox: {
+                runtimeCaching: [
+                    {
+                        urlPattern: ({ url }) => {
+                            return url.pathname.startsWith("/projects");
+                        },
+                        handler: "CacheFirst" as const,
+                        options: {
+                            cacheName: "api-cache",
+                            cacheableResponse: {
+                                statuses: [0, 200],
+                            },
+                        },
+                    },
+                ],
+            },
+        }),
         sentryVitePlugin({
             org: "arbisoft-m1",
             project: "project-listings-frontend",
