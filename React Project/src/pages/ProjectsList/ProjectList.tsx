04ab04f4c8a1a6dba129250cd9ed85b45e3b0668
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-09-11 15:17:25 +0500

    refactor(frontend): move project card to separate component

--- React Project/src/pages/ProjectsList/ProjectList.tsx
+++ React Project/src/pages/ProjectsList/ProjectList.tsx
@@ -10,10 +10,10 @@ import { USER_ROLES } from "../../lib/utils";
 import useAxios from "../../hooks/useAxios";
 import { useMyProjectSubscription, useMyBidSubscription } from "../../hooks/useSubscription";
 
-import Markdown from "react-markdown";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { useAppDispatch, useAppSelector } from "../../redux/store";
 import { selectProjects, setProjects } from "../../redux/projects/slice";
+import ProjectCard from "../../components/ProjectCard";
 
 export default function ProjectsList() {
     const projects = useAppSelector(selectProjects);
@@ -99,53 +99,9 @@ export default function ProjectsList() {
             ) : (
                 <ul id="list" className="flex flex-wrap justify-start gap-2">
                     {searchedProjects?.length ? (
-                        searchedProjects.map(
-                            ({ id, name, description, budget, deadline }, index) => (
-                                <li
-                                    className="relative bg-gray-100 flex flex-col grow justify-between rounded-md p-4 sm:w-full lg:w-1/4 group"
-                                    key={index}
-                                >
-                                    <div>
-                                        <h3 className="text-2xl font-medium">
-                                            {id}: {name}
-                                        </h3>
-                                        <div className="my-4">
-                                            <div className="font-bold">Description:</div>
-                                            <Markdown>
-                                                {description.slice(
-                                                    0,
-                                                    description.indexOf("\n") !== -1
-                                                        ? description.indexOf("\n")
-                                                        : description.length
-                                                )}
-                                            </Markdown>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div className="flex w-full justify-between group-hover:opacity-0 transition duration-500">
-                                            <div>
-                                                <h3 className="font-bold">Budget</h3>
-                                                <div className="bg-cyan-500 rounded-full px-3 mt-1 text-white">
-                                                    {budget} $
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <h3 className="font-bold">Deadline</h3>
-                                                <div className="bg-emerald-500 rounded-full px-3 mt-1 text-white">
-                                                    {new Date(deadline).toDateString()}
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <Link
-                                            to={`/projects/${id}`}
-                                            className="absolute left-0 w-full cursor-pointer bg-gradient-to-r from-cyan-500 to-emerald-500 text-white rounded-md text-center py-2 mt-4 opacity-0 group-hover:-translate-y-full group-hover:opacity-100 transition duration-500 ease-in-out"
-                                        >
-                                            View More
-                                        </Link>
-                                    </div>
-                                </li>
-                            )
-                        )
+                        searchedProjects.map((project) => (
+                            <ProjectCard key={project.id} {...project} />
+                        ))
                     ) : (
                         <div>No projects</div>
                     )}
