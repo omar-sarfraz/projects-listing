10bb28b637cec323d139b8ba12270aa2f1391329
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-09-24 16:57:44 +0500

    deployment(graphql): add dockerfile + change build scripts

--- Graphql_Server/src/lib/db.ts
+++ Graphql_Server/src/lib/db.ts
@@ -8,6 +8,7 @@ const db_name = process.env.DB_NAME;
 const username = process.env.DB_USER;
 const host = process.env.DB_HOST || "localhost";
 const port = parseInt(process.env.BD_PORT || "5432");
+const password = process.env.DB_PASSWORD || "";
 
 if (!db_name || !username) {
     console.error("Database name and Username is required!");
@@ -18,7 +19,7 @@ const db = new Client({
     user: username,
     host: host,
     database: db_name,
-    password: "",
+    password,
     port: port,
 });
 
