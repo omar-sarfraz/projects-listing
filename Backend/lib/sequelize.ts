d597c28b3ba16827dec170588e134b54aa529a8b
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-09-24 16:56:56 +0500

    deployment(express): add dockerfile

--- Backend/lib/sequelize.ts
+++ Backend/lib/sequelize.ts
@@ -3,14 +3,15 @@ import { Sequelize } from "sequelize";
 const db_name = process.env.DB_NAME;
 const username = process.env.DB_USER;
 const host = process.env.DB_HOST || "localhost";
-const port = parseInt(process.env.BD_PORT || "5432");
+const port = parseInt(process.env.DB_PORT || "5432");
+const password = process.env.DB_PASSWORD || "";
 
 if (!db_name || !username) {
     console.error("Database name and Username is required!");
     process.exit(1);
 }
 
-export const sequelize = new Sequelize(db_name, username, "", {
+export const sequelize = new Sequelize(db_name, username, password, {
     host,
     port,
     dialect: "postgres",
