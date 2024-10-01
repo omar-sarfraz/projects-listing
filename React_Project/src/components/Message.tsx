c523740b6dc8c9ae2999a607789a67d380d7da0c
omar-sarfraz <omar.sarfraz@arbisoft.com>
2024-10-01 19:16:12 +0500

    feat(frontend): add infinite scroll pagination in chat

--- React_Project/src/components/Message.tsx
+++ React_Project/src/components/Message.tsx
@@ -1,5 +1,10 @@
 import { MessageType } from "../lib/types";
 
+import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+
+dayjs.extend(localizedFormat);
+
 export default function Message({
     message,
     userId,
@@ -13,11 +18,14 @@ export default function Message({
             key={message.id}
         >
             <div
-                className={`w-[60%] flex px-2 py-2 rounded-sm ${
+                className={`w-[60%] flex flex-col px-2 py-2 rounded-md ${
                     message.userId === userId ? "bg-emerald-500 text-white" : "bg-gray-300"
                 }`}
             >
-                {message.text}
+                <div className="text-lg">{message.text}</div>
+                <div className="w-full text-end text-sm">
+                    {dayjs(message.createdAt).format("LLL")}
+                </div>
             </div>
         </div>
     );
